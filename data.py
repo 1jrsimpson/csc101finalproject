8411cf1ac552340b727dc6059b34bbbcb6bc2351
1jrsimpson <jsimps12@calpoly.edu>
2025-05-31 14:35:03 -0700

    List of 20 songs

--- data.py
+++ data.py
@@ -1,4 +1,26 @@
+# Here is the list of Songs we will be sorting
+from Classes import Song
+from Classes import Duration
 
-class Song
-     def __init__(self):
-         kledlkahwlakwhelmdn
+songs = [
+    Song("Bohemian Rhapsody", Duration(5, 55), 1975, "Queen"),
+    Song("Imagine", Duration(3, 3), 1971, "John Lennon"),
+    Song("Billie Jean", Duration(4, 54), 1982, "Michael Jackson"),
+    Song("Hey Jude", Duration(7, 11), 1968, "The Beatles"),
+    Song("Like a Rolling Stone", Duration(6, 13), 1965, "Bob Dylan"),
+    Song("Smells Like Teen Spirit", Duration(5, 1), 1991, "Nirvana"),
+    Song("Rolling in the Deep", Duration(3, 48), 2010, "Adele"),
+    Song("Blinding Lights", Duration(3, 22), 2019, "The Weeknd"),
+    Song("Shake It Off", Duration(3, 39), 2014, "Taylor Swift"),
+    Song("HUMBLE.", Duration(2, 57), 2017, "Kendrick Lamar"),
+    Song("Lose Yourself", Duration(5, 26), 2002, "Eminem"),
+    Song("Bad Guy", Duration(3, 14), 2019, "Billie Eilish"),
+    Song("Old Town Road", Duration(2, 37), 2019, "Lil Nas X"),
+    Song("Drivers License", Duration(4, 2), 2021, "Olivia Rodrigo"),
+    Song("Uptown Funk", Duration(4, 30), 2014, "Mark Ronson ft. Bruno Mars"),
+    Song("Stay", Duration(2, 21), 2021, "The Kid LAROI & Justin Bieber"),
+    Song("Shape of You", Duration(3, 53), 2017, "Ed Sheeran"),
+    Song("Radioactive", Duration(3, 6), 2012, "Imagine Dragons"),
+    Song("Take Me to Church", Duration(4, 2), 2013, "Hozier"),
+    Song("Can't Feel My Face", Duration(3, 35), 2015, "The Weeknd")
+]
